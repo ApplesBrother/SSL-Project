9e1d9f12371fc222bc40ace03f0108f2ef6e9c67
ApplesBrother <106483090+ApplesBrother@users.noreply.github.com>
2026-03-25 23:15:55 +0530

    Implement user login and account creation in main.sh
    Remaining task: 1v1 not possible

---
+++ main.sh
@@ -0,0 +1,45 @@
+log1=false
+while [ "$log1" = false ]; do
+    echo "First Username: "
+    read user1
+    if [ $(cut -d $'\t' -f1 users.tsv | grep -c "^${user1}$") -eq 1 ]; then
+        echo "Enter password: "
+        read pass1
+        hash1=$(echo -n "$pass1" | sha256sum | cut -d ' ' -f1)
+        if [ $(grep -c "^$user1"$'\t'"$hash1$" users.tsv) -eq 1 ]; then
+            log1=true
+        else
+            echo "Wrong password!! Please try again..."
+        fi
+    else
+        echo "New Username. Enter Password: "
+        read pass1
+        hash1=$(echo -n "$pass1" | sha256sum | cut -d ' ' -f1)
+        echo -e "${user1}\t${hash1}" >> users.tsv
+        echo "New account created!"
+        log1=true
+    fi
+done
+
+log2=false
+while [ "$log2" = false ]; do
+    echo "Second Username: "
+    read user2
+    if [ $(cut -d $'\t' -f1 users.tsv | grep -c "^${user2}$") -eq 1 ]; then
+        echo "Enter password: "
+        read pass2
+        hash2=$(echo -n "$pass2" | sha256sum | cut -d ' ' -f1)
+        if [ $(grep -c "^$user2"$'\t'"$hash2$" users.tsv) -eq 1 ]; then
+            log2=true
+        else
+            echo "Wrong password!! Please try again..."
+        fi
+    else
+        echo "New Username. Enter Password: "
+        read pass2
+        hash2=$(echo -n "$pass2" | sha256sum | cut -d ' ' -f1)
+        echo -e "${user2}\t${hash2}" >> users.tsv
+        echo "New account created!"
+        log2=true
+    fi
+done
