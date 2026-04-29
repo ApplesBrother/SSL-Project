366196f31bdf9d1429fbf6956325dd6bed75cbcb
Aagam Shah <aagam22shah@gmail.com>
2026-04-30 01:58:15 +0530

    Update Historyreset.py

--- Historyreset.py
+++ Historyreset.py
@@ -2,7 +2,7 @@ import csv
 import os
 with open("history.csv", "w",newline="") as history:
     append = csv.writer(history)
-    append.writerow(["Idx", "Time", "Game", "Player 1","Player 2", "Result"])
+    append.writerow(["Idx", "Time", "Game", "Player 1","Player 2", "Result", "Rating1", "Rating2"])
 with open("Serial.txt", "w") as history:
     history.write("0")
 with open("SavedGames.txt", "w") as history:
