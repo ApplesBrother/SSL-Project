2812e5315acaf7c35094a6b155e581664ffdb9a2
ApplesBrother <106483090+ApplesBrother@users.noreply.github.com>
2026-04-29 22:22:03 +0530

    Delete main.sh

--- main.sh
+++
@@ -1,8 +0,0 @@
-echo "First Username: "
-read user1
-if awk -F '\t' -v u="$user1" '$1 == u {found=1} END {exit !found}' users.tsv
-then
-  echo "Username exists"
-else
-  echo "Username not found"
-fi
